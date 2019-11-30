523189c3f61d7dcfb90c98057c4fb24178f5b26a
Joaquim Perez <jquim1@hotmail.com>
2019-11-30 16:50:14 +0000

    cleanup / map fix

--- src/components/App.vue
+++ src/components/App.vue
@@ -11,15 +11,15 @@
             <v-icon>mdi-view-dashboard</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Dashboard</v-list-item-title>
+            <v-list-item-title>Simulations</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item link>
           <v-list-item-action>
-            <v-icon>mdi-settings</v-icon>
+            <v-icon>mdi-history</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Settings</v-list-item-title>
+            <v-list-item-title>History</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -49,7 +49,7 @@
       source: String,
     },
     data: () => ({
-      drawer: null,
+      drawer: false,
     }),
     created () {
       this.$vuetify.theme.dark = true
