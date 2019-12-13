f7168491db05b59bd855539d4ffc3ddcf1fe488f
Joaquim Perez <jquim1@hotmail.com>
2019-12-13 01:44:01 +0000

    routing stuff / map tweaks and filtering

--- src/common/api/ApiHandler.js
+++ src/common/api/ApiHandler.js
@@ -4,19 +4,34 @@ const BASE_DOMAIN = "https://localhost:5001";
 const BASE_URL = `${BASE_DOMAIN}/api/`;
 
 export default
-{
-    getRepository: function (aName) {
-        const oAPIController = axios.create({ baseURL: BASE_URL });
-        return {
-            get: function () {
-                return oAPIController.get(`${aName}`);
-            },
-            getById: function (aId) {
-                return oAPIController.get(`${aName}/${aId}`);
-            },
-            post: function (aData) {
-                return oAPIController.get(`${aName}`, aData);
+    {
+        getRepository: function (aName) {
+            const oAPIController = axios.create({ baseURL: BASE_URL });
+            return {
+                get: function () {
+                    try {
+                        return oAPIController.get(`${aName}`);
+                    }
+                    catch (e) {
+                        return null;
+                    }
+                },
+                getById: function (aId) {
+                    try {
+                        return oAPIController.get(`${aName}/${aId}`);
+                    }
+                    catch (e) {
+                        return null;
+                    }
+                },
+                post: function (aData) {
+                    try {
+                        return oAPIController.get(`${aName}`, aData);
+                    }
+                    catch (e) {
+                        return null;
+                    }
+                }
             }
         }
-    }
-};
+    };
