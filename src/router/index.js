914e20467d8a157dd1bcd301eae28a07501e9cc0
Joaquim Perez <jquim1@hotmail.com>
2019-12-28 18:17:41 +0000

    dynamic route (1)

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-//import Home from '../views/Home.vue'
-//comentei a Home e tou a fazer routing direto do mapa para a raiz
-import faq from '../views/faq.vue'
-//o about foi carregado mesmo no codigo em baixo
-import santarem from '../views/santarem.vue'
-import irs from '../views/irs.vue'
-import liquid from '../views/liquid.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    //name: 'home',
-    //component: Home
-    component: () => import("@/components/Map.vue")
-  },
-  {
-    path: '/faq',
-    name: 'faq',
-    component: faq
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/santarem',
-    name: 'santarem',
-    component: santarem
-  },
-  {
-    path: '/santarem/irs',
-    name: 'irs',
-    component: irs
-  },
-  {
-    path: '/santarem/liquid',
-    name: 'liquid',
-    component: liquid
-  },
-]
+var routes = [
+{
+  path: '/',  
+  component: () => import("@/components/Map.vue")
+}];
+
+const req = require.context('@/views/', true);
+req.keys().forEach(sPath => {
+  const sName = sPath.split('.')[1].split('/')[1];
+  routes.push({
+    path:`/${sName}`,
+    component: () => import(`../views/${sName}.vue`)
+  })
+});
 
 const router = new VueRouter({
   mode: 'history',
