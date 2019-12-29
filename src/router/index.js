97ab5e854347e8f5862b19d055c9bac9bc6340a7
Joaquim Perez <jquim1@hotmail.com>
2019-12-29 17:26:39 +0000

    dynamic routing (based on folder structure)

--- src/router/index.js
+++ src/router/index.js
@@ -3,38 +3,32 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'map',
-    component: () => import("@/views/map.vue")
-  },
-  {
-    path: '/faq',
-    name: 'faq',
-    component: () => import("@/views/faq.vue")
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import("@/views/About.vue")
-  },
-  {
-    path: '/pt/:id',
-    name: 'portugal' ,
-    component: () => import("@/views/portugal/portugal.vue")
-  },
-  {
-    path: '/pt/:id/irsPT',
-    name: 'irsPT',
-    component: () => import("@/views/portugal/irsPT.vue")
-  },
-  {
-    path: '/pt/:id/liquidPT',
-    name: 'liquidPT',
-    component: () => import("@/views/portugal/liquidPT.vue")
-  },
-]
+var routes = [];
+
+const oReq = require.context('@/views/', true, /\.*\.vue$/);
+oReq.keys().forEach(sFilePath => {
+  const sName = sFilePath.substr(2).replace(".vue", "");
+  const bIsSubRoute = sName.indexOf("/") > -1;
+  if (!bIsSubRoute) {
+    const sPath = sName == "map" ? "/" : `/${sName}`;
+    routes.push({
+      path: sPath,
+      name: sName,
+      component: () => import(`../views/${sName}.vue`)
+    })
+  }
+  else {
+    const sSplit = sName.split("/");
+    const sFolderName = sSplit[0];
+    const sChildName = sSplit[1];
+    const sPath = sFolderName == sChildName ? `/${sFolderName}/:id` : `/${sFolderName}/:id/${sChildName}`;
+    routes.push({
+      path: sPath,
+      name: sChildName,
+      component: () => import(`../views/${sFolderName}/${sChildName}.vue`)
+    });
+  }
+});
 
 const router = new VueRouter({
   mode: 'history',
